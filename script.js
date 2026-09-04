6e0b10f8bec70565fa572100a2d88b8fe2c45840
/Babs <seckbabacar974@gmail.com>
2026-09-04 11:08:10 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const ecran = document.getElementById('ecran');
+const boutons = document.querySelectorAll('button');
+
+boutons.forEach(bouton => {
+    bouton.addEventListener('click', () =>{
+        if(bouton.id === 'egal') {
+            ecran.value = eval(ecran.value);
+        } else if (bouton.id === 'clear'){
+           ecran.value = ''; 
+        }else{
+            ecran.value += bouton.innerText;
+        };
+    });
+});
