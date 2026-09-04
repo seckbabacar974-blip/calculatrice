7a689918330873535163ef298a4cdff6de40ae47
/Babs <seckbabacar974@gmail.com>
2026-09-04 16:45:27 +0000

    Ajout du bouton supprimer et du bouton virgule

--- script.js
+++ script.js
@@ -2,13 +2,27 @@ const ecran = document.getElementById('ecran');
 const boutons = document.querySelectorAll('button');
 
 boutons.forEach(bouton => {
-    bouton.addEventListener('click', () =>{
-        if(bouton.id === 'egal') {
+
+    bouton.addEventListener('click', () => {
+
+        if (bouton.id === 'egal') {
+
             ecran.value = eval(ecran.value);
-        } else if (bouton.id === 'clear'){
-           ecran.value = ''; 
-        }else{
+
+        } else if (bouton.id === 'clear') {
+
+            ecran.value = '';
+
+        } else if (bouton.id === 'delete') {
+
+            ecran.value = ecran.value.slice(0, -1);
+
+        } else {
+
             ecran.value += bouton.innerText;
-        };
+
+        }
+
     });
+
 });
